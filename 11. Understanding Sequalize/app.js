1a167f9f17346c4585beb95714e4ac6ea8296fc6
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-02-09 10:19:15 +0530

    Sequalize order model implemented and nosql setup created

--- 11. Understanding Sequalize/app.js
+++ 11. Understanding Sequalize/app.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
-const db = require("./util/database");
 const app = express();
 const Product = require("./models/product");
 const User = require("./models/user");
@@ -14,7 +13,8 @@ const shopRoutes = require("./routes/shop");
 const sequalize = require("./util/database");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
-
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
@@ -38,25 +38,30 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
 sequalize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
   })
   .then((user) => {
-    if (!user) return User.create({ name: "John", email: "john@gmail.com" });
+    if (!user) {
+      return User.create({ name: "Max", email: "test@test.com" });
+    }
     return user;
   })
   .then((user) => {
     // console.log(user);
-    user.createCart();
+    return user.createCart();
   })
-  .then(() => {
+  .then((cart) => {
     app.listen(3000);
-    console.log(`app listening on 3000`);
   })
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
+  .catch((err) => {
+    console.log(err);
   });
