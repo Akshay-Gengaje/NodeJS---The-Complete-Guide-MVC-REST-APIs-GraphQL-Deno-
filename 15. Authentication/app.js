ac69d0ca96da870283037073d3ebe7d31a72c10b
Akshay-Gengaje <Akshaysgengaje@gmail.com>
2024-02-20 19:18:32 +0530

    Authentication initial setup added

--- 15. Authentication/app.js
+++ 15. Authentication/app.js
@@ -1,37 +1,43 @@
 const path = require("path");
-
+const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+
 const errorController = require("./controllers/error");
 const User = require("./models/user");
-const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const store = new MongoDBStore({
   uri: process.env.MONGODB_URL,
   collection: "sessions",
 });
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    secret: "I am the creator",
+    secret: "my secret",
     resave: false,
     saveUninitialized: false,
     store: store,
   })
 );
+
 app.use((req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
@@ -42,6 +48,7 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
 app.use(errorController.get404);
 
 mongoose
